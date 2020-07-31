68ccef7cd3a2a665a0fac055ae58fa5ea44c8cdf
Wlad Gumenyuk <wgumenyuk.gg@gmail.com>
2020-07-31 13:33:31 +0200

    Added PS256, PS384, PS512

--- index.d.ts
+++ index.d.ts
@@ -11,6 +11,7 @@ type Secret = string | Buffer | { key: string; passphrase: string };
 type Algorithm =
     "HS256" | "HS384" | "HS512" |
     "RS256" | "RS384" | "RS512" |
+    "PS256" | "PS384" | "PS512" |
     "ES256" | "ES384" | "ES512" |
     "none";
 
