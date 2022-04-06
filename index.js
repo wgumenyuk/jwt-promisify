8b91447cba9fef68a4bd1235337f56f4360120a9
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-04-07 00:32:44 +0200

    added to export: jsonwebtoken error classes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const { JsonWebTokenError, NotBeforeError, TokenExpiredError } = require("jsonwebtoken");
 const decode = require("./src/decode");
 const sign = require("./src/sign");
 const verify = require("./src/verify");
 
 module.exports = {
+    JsonWebTokenError,
+    NotBeforeError,
+    TokenExpiredError,
     decode,
     sign,
     verify
