e2e710be75eb015ced2bcf94e64304be94502626
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-04-07 14:51:58 +0200

    improved type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,64 +1,81 @@
-/**
-    Type definitions for jwt-promisify.
-    @author Wlad Gumenyuk
+/*
+    Adapted from `@types/jsonwebtoken`.
+    https://github.com/DefinitelyTyped/DefinitelyTyped
 */
 
 // Types
-type Payload = string | object | Buffer;
+export type Key = string | Buffer | { key: string | Buffer; passphrase: string; };
 
-type Secret = string | Buffer | { key: string; passphrase: string };
-
-type Algorithm =
+export type Algorithm =
     "HS256" | "HS384" | "HS512" |
     "RS256" | "RS384" | "RS512" |
     "PS256" | "PS384" | "PS512" |
     "ES256" | "ES384" | "ES512" |
     "none";
 
-// Interfaces
-export interface SignOptions {
-    algorithm?: Algorithm;
-    audience?: string | string[];
-    encoding?: string;
-    /**
-        Expressed in seconds or a string describing a time span [vercel/ms](https://github.com/vercel/ms).
-        E.g. 1000, "2 days", "10h", "7d"
-    */
-    expiresIn?: string | number;
-    header?: object;
-    issuer?: string;
-    jwtid?: string;
-    keyid?: string;
-    noTimestamp?: boolean;
-    /**
-        Expressed in seconds or a string describing a time span [vercel/ms](https://github.com/vercel/ms).
-        E.g. 1000, "2 days", "10h", "7d"
-    */
-    notBefore?: string | number;
-    subject?: string;
-}
+export type Header = {
+    alg: string | Algorithm;
+    typ?: string;
+    cty?: string;
+    crit?: Array<string | Exclude<keyof Header, 'crit'>>;
+    kid?: string;
+    jku?: string;
+    x5u?: string | string[];
+    "x5t#S256"?: string;
+    x5t?: string;
+    x5c?: string | string[];
+};
 
-export interface VerifyOptions {
+export type Payload = {
+    [ key: string ]: any;
+    iss?: string;
+    sub?: string;
+    aud?: string | string[];
+    exp?: number;
+    nbf?: number;
+    iat?: number;
+    jti?: string;
+};
+
+export type Token = {
+    header: Header;
+    payload: Payload | string;
+    signature: string;
+};
+
+export type SignOptions = {
+    algorithm?: Algorithm | undefined;
+    keyid?: string | undefined;
+    expiresIn?: string | number | undefined;
+    notBefore?: string | number | undefined;
+    audience?: string | string[] | undefined;
+    subject?: string | undefined;
+    issuer?: string | undefined;
+    jwtid?: string | undefined;
+    mutatePayload?: boolean | undefined;
+    noTimestamp?: boolean | undefined;
+    header?: Header | undefined;
+    encoding?: string | undefined;
+};
+
+export type VerifyOptions = {
     algorithms?: Algorithm[];
-    audience?: string | RegExp | (string | RegExp)[];
+    audience?: string | RegExp | Array<string | RegExp>;
+    clockTimestamp?: number;
     clockTolerance?: number;
     complete?: boolean;
-    issuer?: string;
+    issuer?: string | string[];
     ignoreExpiration?: boolean;
     ignoreNotBefore?: boolean;
     jwtid?: string;
-    /**
-        Expressed in seconds or a string describing a time span [vercel/ms](https://github.com/vercel/ms).
-        E.g. 1000, "2 days", "10h", "7d"
-    */
-    maxAge?: string | number;
     nonce?: string;
-}
+    subject?: string;
+};
 
-export interface DecodeOptions {
+export type DecodeOptions = {
     complete?: boolean;
     json?: boolean;
-}
+};
 
 // Classes
 export class JsonWebTokenError extends Error {
@@ -67,8 +84,8 @@ export class JsonWebTokenError extends Error {
 }
 
 export class TokenExpiredError extends JsonWebTokenError {
-    expiredAt: number;
-    constructor(message: string, expiredAt: number);
+    expiredAt: Date;
+    constructor(message: string, expiredAt: Date);
 }
 
 export class NotBeforeError extends JsonWebTokenError {
@@ -77,36 +94,24 @@ export class NotBeforeError extends JsonWebTokenError {
 }
 
 // Functions
-/**
-    Signs the payload and returns a token.
-    @param payload              Token payload.
-    @param secretOrPrivateKey   Key or certificate to sign token.
-    @param [options]            Options for the signature.
-*/
-export function sign(
-    payload: Payload,
-    secretOrPrivateKey: Secret,
-    options?: SignOptions
-): Promise<string>;
-
-/**
-    Verifies a token with a secret or public key and returns the payload.
-    @param token                Token to be verified.
-    @param secretOrPublicKeyKey Key or certificate to verify token against.
-    @param [options]            Options for the verification.
-*/
-export function verify(
+export function decode(
     token: string,
-    secretOrPublicKey: Secret,
-    options?: VerifyOptions
-): Promise<Payload>;
+    options: DecodeOptions & { complete: true }
+): Promise<Token | null>;
 
-/**
-    Returns the decoded payload without verifying if the signature is valid.
-    @param token                Token to be decoded.
-    @param [options]            Options for decoding.
-*/
 export function decode(
     token: string,
     options?: DecodeOptions
-): Promise<Payload>;
+): Promise<Record<string, any> | null>;
+
+export function sign(
+    payload: string | object | Buffer,
+    key: Key,
+    options?: SignOptions
+): Promise<string>;
+
+export function verify<T = Record<string, any>>(
+    token: string,
+    key: Key,
+    options?: VerifyOptions
+): Promise<T>;
